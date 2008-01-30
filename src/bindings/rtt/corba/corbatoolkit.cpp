48126057c0100fd305d93df238a1e34c713e0e28
Ruben Smits <ruben.smits@mech.kuleuven.be>
2008-01-30 16:58:23 +0000

    The corba extension code, only for KDL::Vector for now
    git-svn-id: https://svn.mech.kuleuven.be/repos/orocos/trunk/kdl@28893 ce417995-dfc9-0310-95a0-acdaff106893

---
+++ src/bindings/rtt/corba/corbatoolkit.cpp
@@ -0,0 +1,67 @@
+// Copyright  (C)  2008  Ruben Smits <ruben dot smits at mech dot kuleuven dot be>
+
+// Version: 1.0
+// Author: Ruben Smits <ruben dot smits at mech dot kuleuven dot be>
+// Maintainer: Ruben Smits <ruben dot smits at mech dot kuleuven dot be>
+// URL: http://www.orocos.org/kdl
+
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+// Lesser General Public License for more details.
+
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
+#include "../toolkit.hpp"
+#include <rtt/Types.hpp>
+#include <rtt/corba/CorbaTemplateProtocol.hpp>
+#include <rtt/os/StartStopManager.hpp>
+#include "CorbaKDLConversion.hpp"
+
+namespace KDL
+{
+    namespace Corba
+    {
+        using namespace RTT;
+        using namespace RTT::Corba;
+        using namespace RTT::detail;
+        
+        struct CorbaKDLRegistrator
+            : public TransportRegistrator
+        {
+            bool registerTransport(std::string name, TypeInfo* ti)
+            {
+                assert( name == ti->getTypeName() );
+                if ( name == "vector" )
+                    return ti->addProtocol(ORO_CORBA_PROTOCOL_ID, new CorbaTemplateProtocol< KDL::Vector >() );
+                return false;
+            }
+
+            std::string getTransportName() const {
+                return "CORBAKDL";
+            }
+
+        } CorbaKDLRegistrator;
+
+        /**
+         * This struct has the sole purpose of invoking
+         * the Import function.
+         */
+        int loadCorbaKDL()
+        {
+            log(Info) << "Loading CorbaKDL in RTT type system." <<endlog();
+            TypeInfoRepository::Instance()->registerTransport( &CorbaKDLRegistrator );
+            return 0;
+        }
+
+        OS::InitFunction CorbaKDLLoader( &loadCorbaKDL );
+
+    };
+};
