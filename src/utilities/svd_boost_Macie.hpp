c6f201ea821b26f29a55377c916e57105ae09475
Ruben Smits <ruben.smits@mech.kuleuven.be>
2008-03-24 13:14:18 +0000

    Adding missing include of ublas-header
    git-svn-id: https://svn.mech.kuleuven.be/repos/orocos/trunk/kdl@29113 ce417995-dfc9-0310-95a0-acdaff106893

--- src/utilities/svd_boost_Macie.hpp
+++ src/utilities/svd_boost_Macie.hpp
@@ -28,6 +28,7 @@
 #include <boost/numeric/ublas/matrix.hpp>
 #include <boost/numeric/ublas/matrix_proxy.hpp>
 #include <boost/numeric/ublas/vector.hpp>
+#include <boost/numeric/ublas/vector_proxy.hpp>
 
 namespace ublas = boost::numeric::ublas;
 
