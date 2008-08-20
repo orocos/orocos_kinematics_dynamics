2d39bc7cf8d6c8ca20f4d844c360f07b3e1aa613
Ruben Smits <ruben.smits@mech.kuleuven.be>
2008-08-20 08:44:35 +0000

    Added comments
    git-svn-id: https://svn.mech.kuleuven.be/repos/orocos/trunk/kdl@29516 ce417995-dfc9-0310-95a0-acdaff106893

--- src/chainiksolvervel_pinv.cpp
+++ src/chainiksolvervel_pinv.cpp
@@ -44,29 +44,43 @@ namespace KDL
 
     int ChainIkSolverVel_pinv::CartToJnt(const JntArray& q_in, const Twist& v_in, JntArray& qdot_out)
     {
+        //Let the ChainJntToJacSolver calculate the jacobian "jac" for
+        //the current joint positions "q_in" 
         jnt2jac.JntToJac(q_in,jac);
 
+        //Do a singular value decomposition of "jac" with maximum
+        //iterations "maxiter", put the results in "U", "S" and "V"
+        //jac = U*S*Vt
         int ret = svd.calculate(jac,U,S,V,maxiter);
 
         double sum;
         unsigned int i,j;
 
-        // tmp = (Si*U'*Ly*y),
+        // We have to calculate qdot_out = jac_pinv*v_in
+        // Using the svd decomposition this becomes(jac_pinv=V*S_pinv*Ut):
+        // qdot_out = V*S_pinv*Ut*v_in
+
+        //first we calculate Ut*v_in
         for (i=0;i<jac.columns();i++) {
             sum = 0.0;
             for (j=0;j<jac.rows();j++) {
                 sum+= U[j](i)*v_in(j);
             }
+            //If the singular value is to small (<eps), don't invert it but
+            //set the inverted singular value to zero (truncated svd)
             tmp(i) = sum*(fabs(S(i))<eps?0.0:1.0/S(i));
         }
-        // x = Lx^-1*V*tmp + x
+        //tmp is now: tmp=S_pinv*Ut*v_in, we still have to premultiply
+        //it with V to get qdot_out
         for (i=0;i<jac.columns();i++) {
             sum = 0.0;
             for (j=0;j<jac.columns();j++) {
                 sum+=V[i](j)*tmp(j);
             }
+            //Put the result in qdot_out
             qdot_out(i)=sum;
         }
+        //return the return value of the svd decomposition
         return ret;
     }
 
