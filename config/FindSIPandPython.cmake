c95f1ef159284daf72b55e85fa1ae42c48f88a81
Ruben Smits <ruben.smits@mech.kuleuven.be>
2008-10-30 08:30:07 +0000

    fixes bug 594: missing SIP does not disable PYTHON_BINDINGS
    git-svn-id: https://svn.mech.kuleuven.be/repos/orocos/trunk/kdl@29717 ce417995-dfc9-0310-95a0-acdaff106893

--- config/FindSIPandPython.cmake
+++ config/FindSIPandPython.cmake
@@ -41,47 +41,41 @@ IF (PYTHON_LIBRARIES AND PYTHON_INCLUDE_PATH)
     SET (PYTHON_LIBRARIES ${PYTHON_LIBRARIES} util)
   ENDIF (UNIX AND NOT APPLE)
   
-  IF (PYTHON_BINDINGS)
-    
-    # check for SIP
-    TRY_RUN_PYTHON (HAVE_SIP_MODULE "from sip import wrapinstance")
-
-    IF (APPLE)
-      SET (SIP_MAC_PATH
+  # check for SIP
+  TRY_RUN_PYTHON (HAVE_SIP_MODULE "from sip import wrapinstance")
+  
+  IF (APPLE)
+    SET (SIP_MAC_PATH
       /System/Library/Frameworks/Python.framework/Versions/2.5/bin
       /System/Library/Frameworks/Python.framework/Versions/2.4/bin
       /System/Library/Frameworks/Python.framework/Versions/2.3/bin)
-    ENDIF (APPLE)
+  ENDIF (APPLE)
 
-    FIND_PROGRAM (SIP_BINARY_PATH sip PATHS ${SIP_MAC_PATH})
+  FIND_PROGRAM (SIP_BINARY_PATH sip PATHS ${SIP_MAC_PATH})
+  
+  IF (HAVE_SIP_MODULE AND SIP_BINARY_PATH)
+    # check for SIP version
+    # minimal version is 4.4
+    SET (SIP_MIN_VERSION 040400)
+    TRY_RUN_PYTHON (RES "import sip\nprint '%x' % sip.SIP_VERSION" SIP_VERSION)
+    IF (SIP_VERSION EQUAL "${SIP_MIN_VERSION}" OR SIP_VERSION GREATER "${SIP_MIN_VERSION}")
+      SET (SIP_IS_GOOD TRUE)
+    ENDIF (SIP_VERSION EQUAL "${SIP_MIN_VERSION}" OR SIP_VERSION GREATER "${SIP_MIN_VERSION}")
     
-    IF (HAVE_SIP_MODULE AND SIP_BINARY_PATH)
-      # check for SIP version
-      # minimal version is 4.4
-      SET (SIP_MIN_VERSION 040400)
-      TRY_RUN_PYTHON (RES "import sip\nprint '%x' % sip.SIP_VERSION" SIP_VERSION)
-      IF (SIP_VERSION EQUAL "${SIP_MIN_VERSION}" OR SIP_VERSION GREATER "${SIP_MIN_VERSION}")
-        SET (SIP_IS_GOOD TRUE)
-      ENDIF (SIP_VERSION EQUAL "${SIP_MIN_VERSION}" OR SIP_VERSION GREATER "${SIP_MIN_VERSION}")
-    
-      IF (NOT SIP_IS_GOOD)
-        MESSAGE (STATUS "SIP is required in version 4.4 or later!")
-      ENDIF (NOT SIP_IS_GOOD)
-    ELSE (HAVE_SIP_MODULE AND SIP_BINARY_PATH)
-      MESSAGE (STATUS "SIP not found!")
-    ENDIF (HAVE_SIP_MODULE AND SIP_BINARY_PATH)
-     
-    # if SIP and PyQt4 are found, enable bindings
-    IF (SIP_IS_GOOD)
-      SET (HAVE_PYTHON TRUE)
-      MESSAGE(STATUS "Python bindings enabled")
-    ELSE (SIP_IS_GOOD)
-      SET (HAVE_PYTHON FALSE)
-      MESSAGE(STATUS "Python bindings disabled due dependency problems!")
-    ENDIF (SIP_IS_GOOD)
-    
-  ELSE (PYTHON_BINDINGS)
-    MESSAGE(STATUS "Python bindings disabled")
-  ENDIF (PYTHON_BINDINGS)
-
+    IF (NOT SIP_IS_GOOD)
+      MESSAGE (STATUS "SIP is required in version 4.4 or later!")
+    ENDIF (NOT SIP_IS_GOOD)
+  ELSE (HAVE_SIP_MODULE AND SIP_BINARY_PATH)
+    MESSAGE (STATUS "SIP not found!")
+  ENDIF (HAVE_SIP_MODULE AND SIP_BINARY_PATH)
+  
+  # if SIP and PyQt4 are found, enable bindings
+  IF (SIP_IS_GOOD)
+    SET (HAVE_PYTHON TRUE)
+    MESSAGE(STATUS "Python bindings enabled")
+  ELSE (SIP_IS_GOOD)
+    SET (HAVE_PYTHON FALSE)
+    MESSAGE(STATUS "Python bindings disabled due dependency problems!")
+  ENDIF (SIP_IS_GOOD)
+  
 ENDIF (PYTHON_LIBRARIES AND PYTHON_INCLUDE_PATH)
