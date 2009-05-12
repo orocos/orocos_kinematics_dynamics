fdb7115266fc84e234f99238a89c3bbce34822ed
Ruben Smits <ruben.smits@mech.kuleuven.be>
2009-05-12 23:36:24 +0000

    Fix bug #652:  python bindings fail to compile if boost-ublas is not installed in a standard path
    git-svn-id: https://svn.mech.kuleuven.be/repos/orocos/trunk/kdl@30149 ce417995-dfc9-0310-95a0-acdaff106893

--- config/FindBoostUblas.cmake
+++ config/FindBoostUblas.cmake
@@ -1,7 +1,7 @@
 FIND_PATH(BOOST boost/numeric/ublas/matrix.hpp  )
 IF ( BOOST )
     MESSAGE(STATUS "-- Looking for Boost Ublas - found")
-	INCLUDE_DIRECTORIES( ${BOOST} )
+    SET(KDL_CFLAGS "${KDL_CFLAGS} -I${BOOST}" CACHE INTERNAL "")
 ELSE ( BOOST )
     MESSAGE(FATAL "-- Looking for Boost Ublas - not found")
 ENDIF ( BOOST )
