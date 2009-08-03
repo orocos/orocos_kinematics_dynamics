8fb3ceaa8c43a3f0d682c963f2cbbeecf48a00ba
Ruben Smits <ruben.smits@mech.kuleuven.be>
2009-08-03 07:35:22 +0000

    Updated SIP detection, hopefully fixes OSX issues
    git-svn-id: https://svn.mech.kuleuven.be/repos/orocos/trunk/kdl@30396 ce417995-dfc9-0310-95a0-acdaff106893

--- config/FindSIPandPython.cmake
+++ config/FindSIPandPython.cmake
@@ -6,31 +6,6 @@
 FIND_PACKAGE(PythonLibs) # MapServer export tool
 FIND_PACKAGE(PythonInterp) # test for sip and PyQt4
 
-
-MACRO (TRY_RUN_PYTHON RESULT CMD)
-  IF (PYTHONINTERP_FOUND)
-    
-    EXEC_PROGRAM(${PYTHON_EXECUTABLE} ARGS -c "\"${CMD}\""
-                 OUTPUT_VARIABLE out
-                 RETURN_VALUE retval)
-    
-    # optional last parameter to save the output
-    SET (OUTPUT ${ARGV2})
-    IF (OUTPUT)
-      SET(${OUTPUT} ${out})
-    ENDIF (OUTPUT)
-    
-    IF (retval EQUAL 0)
-      SET (${RESULT} TRUE)
-    ELSE (retval EQUAL 0)
-      SET (${RESULT} FALSE)
-    ENDIF (retval EQUAL 0)
-  
-  ELSE (PYTHONINTERP_FOUND)
-    SET (${RESULT} FALSE)
-  ENDIF (PYTHONINTERP_FOUND)
-ENDMACRO (TRY_RUN_PYTHON)
-
 # enable/disable python support (mapserver export tool and bindings)
 IF (PYTHON_LIBRARIES AND PYTHON_INCLUDE_PATH)
   SET (PYTHON_FOUND TRUE)
@@ -40,36 +15,25 @@ IF (PYTHON_LIBRARIES AND PYTHON_INCLUDE_PATH)
   IF (UNIX AND NOT APPLE)
     SET (PYTHON_LIBRARIES ${PYTHON_LIBRARIES} util)
   ENDIF (UNIX AND NOT APPLE)
-  
-  # check for SIP
-  TRY_RUN_PYTHON (HAVE_SIP_MODULE "from sip import wrapinstance")
-  
-  IF (APPLE)
-    SET (SIP_MAC_PATH
-      /System/Library/Frameworks/Python.framework/Versions/2.5/bin
-      /System/Library/Frameworks/Python.framework/Versions/2.4/bin
-      /System/Library/Frameworks/Python.framework/Versions/2.3/bin)
-  ENDIF (APPLE)
-
-  FIND_PROGRAM (SIP_BINARY_PATH sip PATHS ${SIP_MAC_PATH})
-  
-  IF (HAVE_SIP_MODULE AND SIP_BINARY_PATH)
+  SET(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} ${PROJ_SOURCE_DIR}/config)
+  FIND_PACKAGE(SIP)
+  #INCLUDE(${PROJ_SOURCE_DIR}/config/FindSIP.cmake)
+  IF (SIP_FOUND)
     # check for SIP version
     # minimal version is 4.4
-    SET (SIP_MIN_VERSION 040400)
-    TRY_RUN_PYTHON (RES "import sip\nprint '%x' % sip.SIP_VERSION" SIP_VERSION)
+    SET (SIP_MIN_VERSION 040700)
     IF (SIP_VERSION EQUAL "${SIP_MIN_VERSION}" OR SIP_VERSION GREATER "${SIP_MIN_VERSION}")
       SET (SIP_IS_GOOD TRUE)
     ENDIF (SIP_VERSION EQUAL "${SIP_MIN_VERSION}" OR SIP_VERSION GREATER "${SIP_MIN_VERSION}")
     
     IF (NOT SIP_IS_GOOD)
-      MESSAGE (STATUS "SIP is required in version 4.4 or later!")
+      MESSAGE (STATUS "SIP is required in version 4.7 or later!")
     ENDIF (NOT SIP_IS_GOOD)
-  ELSE (HAVE_SIP_MODULE AND SIP_BINARY_PATH)
+  ELSE (SIP_FOUND)
     MESSAGE (STATUS "SIP not found!")
-  ENDIF (HAVE_SIP_MODULE AND SIP_BINARY_PATH)
+  ENDIF (SIP_FOUND)
   
-  # if SIP and PyQt4 are found, enable bindings
+  # if SIP is found, enable bindings
   IF (SIP_IS_GOOD)
     SET (HAVE_PYTHON TRUE)
     MESSAGE(STATUS "Python bindings enabled")
