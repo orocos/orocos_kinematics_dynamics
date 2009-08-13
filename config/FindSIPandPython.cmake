8a3f0b94f49a56a6c2844ab71eeaf1df6dd39ceb
Ruben Smits <ruben.smits@mech.kuleuven.be>
2009-08-13 09:05:44 +0000

    reworked python/sip cmake logic
    git-svn-id: https://svn.mech.kuleuven.be/repos/orocos/trunk/kdl@30427 ce417995-dfc9-0310-95a0-acdaff106893

--- config/FindSIPandPython.cmake
+++ config/FindSIPandPython.cmake
@@ -2,9 +2,8 @@
 # there is a two-stage support for python:
 # - 
 
-
-FIND_PACKAGE(PythonLibs) # MapServer export tool
-FIND_PACKAGE(PythonInterp) # test for sip and PyQt4
+SET(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} ${PROJ_SOURCE_DIR}/config)
+FIND_PACKAGE(PythonLibrary)
 
 # enable/disable python support (mapserver export tool and bindings)
 IF (PYTHON_LIBRARIES AND PYTHON_INCLUDE_PATH)
@@ -15,19 +14,18 @@ IF (PYTHON_LIBRARIES AND PYTHON_INCLUDE_PATH)
   IF (UNIX AND NOT APPLE)
     SET (PYTHON_LIBRARIES ${PYTHON_LIBRARIES} util)
   ENDIF (UNIX AND NOT APPLE)
-  SET(CMAKE_MODULE_PATH ${CMAKE_MODULE_PATH} ${PROJ_SOURCE_DIR}/config)
   FIND_PACKAGE(SIP)
   #INCLUDE(${PROJ_SOURCE_DIR}/config/FindSIP.cmake)
   IF (SIP_FOUND)
     # check for SIP version
     # minimal version is 4.4
-    SET (SIP_MIN_VERSION 040700)
+    SET (SIP_MIN_VERSION 040709)
     IF (SIP_VERSION EQUAL "${SIP_MIN_VERSION}" OR SIP_VERSION GREATER "${SIP_MIN_VERSION}")
       SET (SIP_IS_GOOD TRUE)
     ENDIF (SIP_VERSION EQUAL "${SIP_MIN_VERSION}" OR SIP_VERSION GREATER "${SIP_MIN_VERSION}")
     
     IF (NOT SIP_IS_GOOD)
-      MESSAGE (STATUS "SIP is required in version 4.7 or later!")
+      MESSAGE (STATUS "SIP is required in version 4.7.9 or later!")
     ENDIF (NOT SIP_IS_GOOD)
   ELSE (SIP_FOUND)
     MESSAGE (STATUS "SIP not found!")
