e426c22909d0539748bde78d03b94bd879c6ed61
Ruben Smits <ruben.smits@mech.kuleuven.be>
2011-05-11 07:12:21 +0200

    eigen3: fix copying from/into symmetric matrices
    Signed-off-by: Ruben Smits <ruben.smits@mech.kuleuven.be>

--- orocos_kdl/src/articulatedbodyinertia.cpp
+++ orocos_kdl/src/articulatedbodyinertia.cpp
@@ -27,10 +27,11 @@ using namespace Eigen;
 
 namespace KDL{
     
-    ArticulatedBodyInertia::ArticulatedBodyInertia(const RigidBodyInertia& rbi)
+  ArticulatedBodyInertia::ArticulatedBodyInertia(const RigidBodyInertia& rbi):
+    M(Matrix3d::Zero()),I(Matrix3d::Zero()),H(Matrix3d::Zero())
     {
-        this->M.triangularView<Lower>()=Matrix3d::Identity()*rbi.m;
-        this->I.triangularView<Lower>()=Map<Matrix3d>((double*)rbi.I.data);
+        this->M=Matrix3d::Identity()*rbi.m;
+        this->I=Map<Matrix3d>((double*)rbi.I.data);
         this->H << 0,-rbi.h[2],rbi.h[1],
             rbi.h[2],0,-rbi.h[0],
             -rbi.h[1],rbi.h[0],0;
@@ -41,10 +42,11 @@ namespace KDL{
         *this = RigidBodyInertia(m,c,Ic);
     }
 
-    ArticulatedBodyInertia::ArticulatedBodyInertia(const Matrix3d& M, const Matrix3d& H, const Matrix3d& I)
+  ArticulatedBodyInertia::ArticulatedBodyInertia(const Matrix3d& M, const Matrix3d& H, const Matrix3d& I):
+    M(Matrix3d::Zero()),I(Matrix3d::Zero()),H(Matrix3d::Zero())
     {
-        this->M.triangularView<Lower>()=M;
-        this->I.triangularView<Lower>()=I;
+        this->M=M;
+        this->I=I;
         this->H=H;
     }
     
