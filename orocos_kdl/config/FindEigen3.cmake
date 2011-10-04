b525e64b962379b9a0a6bd10e50a4808816b298e
Ruben Smits <ruben.smits@mech.kuleuven.be>
2011-10-04 16:14:01 +0200

    cmake: fall back on own cmake module if the system does not provide a module for eigen3
    Signed-off-by: Ruben Smits <ruben.smits@mech.kuleuven.be>

--- orocos_kdl/config/FindEigen3.cmake
+++ orocos_kdl/config/FindEigen3.cmake
@@ -1,4 +1,4 @@
-FIND_PATH(EIGEN3_INCLUDE_DIR Eigen/Core /usr/include /usr/include/eigen3)
+FIND_PATH(EIGEN3_INCLUDE_DIR Eigen/Core /usr/include /usr/include/eigen3 /usr/local/include /usr/local/include/eigen3)
 IF ( EIGEN3_INCLUDE_DIR )
     MESSAGE(STATUS "-- Looking for Eigen3 - found")
     SET(KDL_CFLAGS "${KDL_CFLAGS} -I${EIGEN3_INCLUDE_DIR}" CACHE INTERNAL "")
