7f269e3e2fd838aa6d9caed3f6dbe2ae0f974c33
Jonathan Bohren <jbo@jhu.edu>
2013-09-25 16:58:33 -0400

    fixing leftover 'orocos_kdl's

--- python-orocos-kdl/doc/conf.py
+++ python-orocos-kdl/doc/conf.py
@@ -13,7 +13,7 @@
 
 import sys, os
 import roslib
-roslib.load_manifest('python_orocos_kdl')
+roslib.load_manifest('python_orocos-kdl')
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
