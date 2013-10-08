2260c70e3dfcc54d476ed765fea0be3e2c92a8b7
Jonathan Bohren <jbo@jhu.edu>
2013-10-08 16:18:41 -0400

    updating documentation, fixing some deps, install targets

--- pykdl/doc/conf.py
+++ pykdl/doc/conf.py
@@ -13,7 +13,7 @@
 
 import sys, os
 import roslib
-roslib.load_manifest('python_orocos-kdl')
+roslib.load_manifest('pykdl')
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
