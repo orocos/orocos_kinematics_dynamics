4d3dfd2674a59d43db7252fe11f64864cf841739
Serge Nikulin <serge.nikulin@apex.ai>
2017-10-09 08:21:39 -0700

    Check buffer size, fix strncpy

--- orocos_kdl/src/utilities/error_stack.cxx
+++ orocos_kdl/src/utilities/error_stack.cxx
@@ -48,11 +48,17 @@ void IOTraceOutput(std::ostream& os) {
 
 
 void IOTracePopStr(char* buffer,int size) {
+    if (size <= 0)
+    {
+        // TODO: all sizes everywhere should be of size_t!
+        return;
+    }
     if (errorstack.empty()) {
         *buffer = 0;
         return;
     }
     strncpy(buffer,errorstack.top().c_str(),size);
+    buffer[size - 1] = `\0`;
     errorstack.pop();
 }
 
