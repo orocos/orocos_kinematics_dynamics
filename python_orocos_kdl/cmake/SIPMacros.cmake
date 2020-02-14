09acd10e418e3c75b30b4b9c661189dea5aa14b0
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2020-02-14 20:31:08 +0100

    Depend on all SIP includes, to trigger a rebuild if any of the sip files changed

--- python_orocos_kdl/cmake/SIPMacros.cmake
+++ python_orocos_kdl/cmake/SIPMacros.cmake
@@ -97,7 +97,7 @@ MACRO(ADD_SIP_PYTHON_MODULE MODULE_NAME MODULE_SIP)
         COMMAND ${CMAKE_COMMAND} -E echo ${message}
         COMMAND ${CMAKE_COMMAND} -E touch ${_sip_output_files}
         COMMAND ${SIP_EXECUTABLE} ${_sip_tags} ${_sip_x} ${SIP_EXTRA_OPTIONS} -j ${SIP_CONCAT_PARTS} -c ${CMAKE_CURRENT_SIP_OUTPUT_DIR} ${_sip_includes} ${_abs_module_sip}
-        DEPENDS ${_abs_module_sip} ${SIP_EXTRA_FILES_DEPEND}
+        DEPENDS ${SIP_INCLUDES} ${SIP_EXTRA_FILES_DEPEND}
     )
     # not sure if type MODULE could be uses anywhere, limit to cygwin for now
     IF (CYGWIN OR APPLE)
