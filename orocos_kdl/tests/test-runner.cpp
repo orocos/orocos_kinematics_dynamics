a73828cb485d3d9eb9e809f882f412469f2f38ad
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2021-06-18 15:21:02 +0200

    Remove wrong license

--- orocos_kdl/tests/test-runner.cpp
+++ orocos_kdl/tests/test-runner.cpp
@@ -1,21 +1,6 @@
-// Copyright (C) 2007 Klaas Gadeyne <first dot last at gmail dot com>
-//
-// This program is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation; either version 2 of the License, or
-// (at your option) any later version.
-//
-// This program is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-//
-// You should have received a copy of the GNU General Public License
-// along with this program; if not, write to the Free Software
-// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
-//
+// Copyright (C) 2007 Klaas Gadeyne
 
-/* Modified from orocos cppunit test code, also published under GPLV2
+/* Modified from orocos cppunit test code
     begin                : Mon January 10 2005
     copyright            : (C) 2005 Peter Soetens
     email                : peter.soetens@mech.kuleuven.ac.be
