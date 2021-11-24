110e50bf9e1acd927b70e84d86ab8f2b6fcb105a
craigirobot <craigc@ssl.umd.edu>
2021-11-24 10:27:12 -0500

    Update orocos_kdl/src/chainiksolverpos_nr.cpp
    Co-authored-by: Matthijs van der Burgh <matthijs.vander.burgh@live.nl>

--- orocos_kdl/src/chainiksolverpos_nr.cpp
+++ orocos_kdl/src/chainiksolverpos_nr.cpp
@@ -53,8 +53,8 @@ namespace KDL
         q_out = q_init;
         bool degraded = false;
 
-        for(numiter=1;numiter<=maxiter;numiter++){
-            degraded = false;        // prove otherwise
+        for(numiter=1; numiter<=maxiter; ++numiter) {
+            degraded = false; // prove otherwise
             if (E_NOERROR > fksolver.JntToCart(q_out,f) )
                 return (error = E_FKSOLVERPOS_FAILED);
             delta_twist = diff(f,p_in);
