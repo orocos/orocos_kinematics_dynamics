f96e321188fe249be2ca8ced53c7f627e004561b
Oleg Goncharov <goncharovoi@yandex.ru>
2024-12-07 19:32:17 +0300

    Add python binding for Motion classes.
    Bindings for RotationalInterpolation, Path, Trajectory, VelocityProfile
    and their subclasses are added.
    
    * To avoid problems with refrences counting and lifetime composite objects
      always stores copies objects (aggregate falg is set).
    * Read() functions bindings are not implemented.

--- python_orocos_kdl/PyKDL/PyKDL.h
+++ python_orocos_kdl/PyKDL/PyKDL.h
@@ -33,3 +33,4 @@ void init_frames(pybind11::module &m);
 void init_framevel(pybind11::module &m);
 void init_kinfam(pybind11::module &m);
 void init_dynamics(pybind11::module &m);
+void init_motion(pybind11::module &m);
